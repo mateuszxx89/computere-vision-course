b654c7edc4cfb2decead9df01bf5ab45f818eb53
Mateusz <piskulak.mateusz@gmail.com>
2024-10-09 21:43:48 +0200

    new files added2

--- 01_basics/04_color_palette.py
+++ 01_basics/04_color_palette.py
@@ -5,13 +5,18 @@ import  numpy as np
 def nothing(x):
     pass
 
+def callback_function(value):
+    print("Suwak przesunięty! Nowa wartość:", value)
+
+
+
+
 
 img = np.zeros(shape=(300, 500, 3), dtype='uint8')
 cv2.namedWindow('Paleta')
-
-cv2.createTrackbar('Red', 'Paleta', 0, 255, nothing)
-cv2.createTrackbar('Green', 'Paleta', 0, 255, nothing)
-cv2.createTrackbar('Blue', 'Paleta', 0, 255, nothing)
+cv2.createTrackbar('Red', 'Paleta', 0, 255, callback_function)
+cv2.createTrackbar('Green', 'Paleta', 0, 255, callback_function)
+cv2.createTrackbar('Blue', 'Paleta', 0, 255, callback_function)
 
 while True:
     cv2.imshow('Paleta', img)
